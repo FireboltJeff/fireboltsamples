1abe233e2d66c516d771a0ae9a0d689e2a98fd6e
FireboltJeff <83974421+FireboltJeff@users.noreply.github.com>
2021-07-27 07:42:29 -0700

    Update select-lambda-snippet-cdw-whitepaper.sql

--- select-lambda-snippet-cdw-whitepaper.sql
+++ select-lambda-snippet-cdw-whitepaper.sql
@@ -1,8 +1,12 @@
-SELECT COUNT(id) as number_of_visits FROM visits
-   UNNEST (events_type)
-   WHERE
-      any_match({(prop_name, prop_val)-> prop_name = ‘agent_string’ AND
-      prop_val LIKE % ‘safari’}, user_agent_properties_names,
-      user_agent_properties_values)
-   GROUP BY events_type
-   ORDER BY number_of_visits
+SELECT 
+   COUNT(id) AS number_of_visits 
+FROM 
+   visits
+UNNEST (events_type)
+WHERE
+       any_match({(prop_name, prop_val)-> prop_name = ‘agent_string’ 
+   AND prop_val LIKE % ‘safari’}, user_agent_properties_names, user_agent_properties_values)
+GROUP BY 
+   events_type
+ORDER BY 
+   number_of_visits

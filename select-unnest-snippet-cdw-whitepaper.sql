386f587c4afdd0ce34ba2b0bbf728b12de48b028
FireboltJeff <83974421+FireboltJeff@users.noreply.github.com>
2021-07-27 07:47:45 -0700

    Update select-unnest-snippet-cdw-whitepaper.sql

--- select-unnest-snippet-cdw-whitepaper.sql
+++ select-unnest-snippet-cdw-whitepaper.sql
@@ -1,5 +1,12 @@
-SELECT id FROM visits
-   UNNEST (tags)
-   WHERE tags = ‘sale’ OR tags = ‘fashion’
-   GROUP BY id
-   ORDER BY duration DESC
+SELECT 
+   id 
+FROM 
+   visits
+UNNEST (tags)
+WHERE 
+      tags = ‘sale’ 
+   OR tags = ‘fashion’
+GROUP BY 
+   id
+ORDER BY 
+   duration DESC
